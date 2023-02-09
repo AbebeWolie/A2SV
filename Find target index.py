db77f8ab39c3d5d241c68ac6e776a262d0ab5388
AbebeWolie <106948479+AbebeWolie@users.noreply.github.com>
2023-02-09 21:04:58 +0300

    Create Find target index.py

---
+++ Find target index.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/find-target-indices-after-sorting-array
+
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        nums.sort()
+        res=[]
+        for i in range(len(nums)):
+            if(target==nums[i]):
+                res.append(i)
+        return res
